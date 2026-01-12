617ef4384ee5c7399797194c9ee8e791b56e7040
xXx_Elock2012_xXx <lucian.ion45@yahoo.com>
2026-01-12 12:11:21 +0200

    Cleaned main for interview

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include "Engine.hpp"
 
 int main() {
 
-    Engine simulation;
-    simulation.Setup();
-    simulation.Run();
 
-    return -1;
+    return 0;
 }
 
